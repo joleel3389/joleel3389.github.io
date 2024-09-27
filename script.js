7a5d6ef8bde4caf7384cb859d7531df03eb23a4a
joleel3389 <joleel@nycstudents.net>
2024-09-27 14:00:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Jolee Liu"; // update the part BETWEEN the "quotes"
+let userName = "joleel3389"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
